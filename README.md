2620709f678d35596f825d80ab6c64cdfe00f289
Boris Skert <boris.skert@gmail.com>
2022-04-09 17:50:51 +0200

    Run tests in Github Actions CI pipeline

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Python katas
+
+![](https://www.codewars.com/users/besterboris/badges/micro)
+
+This repo contains my solutions for some Python katas submitted in [Codewars](https://www.codewars.com).
+
+## Prerequisites
+
+You need to install the [Python 3.8](https://www.python.org/) for your OS to build this project and run its tests.
+
+## Install dependencies
+
+```shell
+pip install -r requirements.txt
+```
+
+## Run tests
+
+```shell
+python run_tests.py
+```
---
+++ run_tests.py
@@ -0,0 +1,28 @@
+import subprocess
+import sys
+
+
+def run_tests():
+    result = subprocess.run(['python', '-m', 'unittest'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+
+    lines = output.split("\n")
+
+    failed_lines = list(filter(has_failed, lines))
+
+    if len(failed_lines) > 0:
+        error_output = "\n".join(failed_lines)
+        print(error_output)
+        sys.exit(1)
+
+    success_output = "\n".join(lines)
+    print(success_output)
+    sys.exit(0)
+
+
+def has_failed(line):
+    return "<FAILED::>" in line
+
+
+if __name__ == "__main__":
+    run_tests()
