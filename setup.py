680a6ea3923c3b064244d13091411b337e322aae
Boris Skert <boris.skert@gmail.com>
2022-04-09 16:55:06 +0200

    Setup project

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="python_katas",
+    version="0.1.0",
+    packages=[],
+    license="MIT",
+    description="My solutions for some Python katas submitted in Codewars.",
+    url="https://github.com/borisskert/python-katas",
+    install_requires=[
+        "codewars_test"
+    ],
+    dependency_links=[]
+)
